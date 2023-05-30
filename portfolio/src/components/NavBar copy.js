f42d5908775665ec317427d1bf1091b2093b4029
RINO-GAELICO <ducciorocca@gmail.com>
2023-05-30 01:37:11 -0700

    Added the navBar and polished it

---
+++ portfolio/src/components/NavBar copy.js
@@ -0,0 +1,111 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useState, useEffect } from "react";
+import logo from "../assets/img/Duccio Rocca-1.png";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+
+export const NavBar = () => {
+  // this is the state for the active link
+  // useState is a hook that allows you to have state variables in functional components
+  // in this case, we are setting the initial state of activeLink to 'home'
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (event, link) => {
+    // make sure to re-render the navbar after we update the active link
+
+    // this is how you do
+    event.preventDefault();
+    
+    setActiveLink(link);
+    
+  };
+
+  return (
+    <Navbar className={scrolled ? "scrolled" : ""} expand="lg">
+      <Container>
+        <a
+          href="#home" 
+          onClick={(event) => onUpdateActiveLink(event,"home")}
+          style={{ textDecoration: "none" }}
+        >
+          <span className="duccio">Duccio Rocca</span>
+        </a>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              id="home"
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={(event) => onUpdateActiveLink(event,"home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              id="skills"
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={(event) => onUpdateActiveLink(event,"skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              id="projects"
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link "
+              }
+              onClick={(event) => onUpdateActiveLink(event,"projects")}
+            >
+              Projects
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a
+                href="https://www.linkedin.com/in/duccio-rocca/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon1} alt="LinkedIn Logo" />
+              </a>
+              <a
+                href="https://github.com/RINO-GAELICO"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon2} alt="GitHub Logo" />
+              </a>
+
+              <button className="vvd" onClick={() => console.log("connect")}>
+                <span>Let's Connect</span>{" "}
+              </button>
+            </div>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
